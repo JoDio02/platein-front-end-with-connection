2b709f40b8fa6913c1d0662f7773bfa00ce87f6f
Alper Ceyhun <80386511+AlperCeyhun@users.noreply.github.com>
2024-11-30 20:21:56 +0300

    added register to blacklist

--- src/components/sidebar/SidebarContainer.tsx
+++ src/components/sidebar/SidebarContainer.tsx
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
 
 export default function SidebarContainer() {
   const pathname = usePathname();
-  const ShouldRender = !["/login", "/register"].includes(pathname);
+  const ShouldRender = !pathname.startsWith("/register") && !pathname.startsWith("/login");
 
   if (!ShouldRender) return null;
   return <Sidebar />;
