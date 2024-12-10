87e9fc57c902392b837cc9cb18faad389bd23d48
Alper Ceyhun <80386511+AlperCeyhun@users.noreply.github.com>
2024-12-10 13:41:56 +0300

    created apiRequest util

---
+++ src/utils/api/ApiRequest.ts
@@ -0,0 +1,37 @@
+interface ApiRequestOptions<T = any> {
+    endpoint: string;
+    bodyData: T;
+    router: any;
+    successRoute: string;
+    onError?: (error: any) => void;
+  }
+  
+  export const apiRequest = async <T = any>({
+    endpoint,
+    bodyData,
+    router,
+    successRoute,
+    onError,
+  }: ApiRequestOptions<T>) => {
+    try {
+      const response = await fetch(endpoint, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bodyData),
+      });
+  
+      if (response.ok) {
+        router.push(successRoute);
+      } else {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "An error occurred.");
+      }
+    } catch (error) {
+      if (onError) onError(error);
+      else console.error("API Request Error:", error);
+    }
+  };
+  
