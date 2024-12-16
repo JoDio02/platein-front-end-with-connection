e41eb9f4eb9a88f0447af7bcfe4ea4f7a3bc1779
Alper Ceyhun <80386511+AlperCeyhun@users.noreply.github.com>
2024-12-16 22:19:21 +0300

    dummy update on addmeal

--- src/components/meals/AddMeal.tsx
+++ src/components/meals/AddMeal.tsx
@@ -7,9 +7,9 @@ import {CirclePlus} from "lucide-react"
 const AddMeal = () => {
     return (
         <div>
-            <button className="btn btn-primary w-full">
-                Add new meal
-                <CirclePlus size={6}/>
+            <button className="btn btn-primary w-full border-indigo-200 flex items-center">
+                <h1 className="">Add new meal</h1>
+                <CirclePlus size={18}/>
             </button>
         </div>
     )
